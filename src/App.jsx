f9100a23bf59dc293db2e10adebe0a68ef692691
Dranyk <dvdranyk@gmail.com>
2023-03-16 18:49:02 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useEffect, lazy, Suspense } from 'react';
 import { useDispatch } from 'react-redux';
-import { Route, Routes, Outlet } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import Loader from 'components/Loader/Loader';
 import LoginPage from 'pages/LoginPage';
 import RegisterPage from 'pages/RegisterPage';
@@ -23,22 +23,21 @@ const App = () => {
 
     return (
         <Suspense fallback={<Loader />}>
-      {
-        <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route index element={<HomePage />} />
-            <Route element={<PublicRoute />}>
-              <Route path="/register" element={<RegisterPage />} />
-              <Route path="/login" element={<LoginPage />} />
-            </Route>
-            <Route element={<PrivateRoute />}>
-              <Route path="/contacts" element={<PhonebookPage />} />
-            </Route>
-          </Route>
-            {/* <Route path="/contacts" element={<PhonebookPage />} /> */}
-        </Routes>
-      }
-    </Suspense>
+        {!isRefreshing && (
+            <Routes>
+              <Route path="/" element={<Layout />}>
+                <Route index element={<HomePage />} />
+                <Route element={<PublicRoute />}>
+                  <Route path="/register" element={<RegisterPage />} />
+                  <Route path="/login" element={<LoginPage />} />
+                </Route>
+                <Route element={<PrivateRoute />}>
+                  <Route path="/contacts" element={<PhonebookPage />} />
+                </Route>
+              </Route>
+            </Routes>
+          )}
+        </Suspense>
     )
 }
 
