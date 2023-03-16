a6da9688184b25d862654dcaaec1058f25e056c8
Dranyk <dvdranyk@gmail.com>
2023-03-16 18:39:33 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,25 @@
 import { useEffect, lazy, Suspense } from 'react';
 import { useDispatch } from 'react-redux';
 import { Route, Routes, Outlet } from 'react-router-dom';
-import PhonebookPage from './pages/PhonebookPage';
-import NavBar from './components/NavBar/NavBar';
 import Loader from 'components/Loader/Loader';
 import LoginPage from 'pages/LoginPage';
 import RegisterPage from 'pages/RegisterPage';
-import HomePage from 'pages/HomePage';
+import { useAuth } from 'hooks/useAuth';
 import { Layout } from 'components/Layout/Layout';
 import PublicRoute from 'components/PublicRoute/PublicRoute';
+import PrivateRoute from 'components/PrivateRoute/PrivateRoute';
+import { refreshUser } from 'redux/auth/authOperations';
 
+const HomePage = lazy(() => import('./pages/HomePage'));
+const PhonebookPage = lazy(() => import('./pages/PhonebookPage'));
 
 const App = () => {
     const dispatch = useDispatch();
-    // const { isRefreshing } = useAuth();
+    const { isRefreshing } = useAuth();
   
-    // useEffect(() => {
-    //   dispatch(refreshUser());
-    // }, [dispatch]);
+    useEffect(() => {
+      dispatch(refreshUser());
+    }, [dispatch]);
 
     return (
         <Suspense fallback={<Loader />}>
@@ -29,11 +31,11 @@ const App = () => {
               <Route path="/register" element={<RegisterPage />} />
               <Route path="/login" element={<LoginPage />} />
             </Route>
-            {/* <Route element={<PrivateRoute />}>
+            <Route element={<PrivateRoute />}>
               <Route path="/contacts" element={<PhonebookPage />} />
-            </Route> */}
+            </Route>
           </Route>
-            <Route path="/contacts" element={<PhonebookPage />} />
+            {/* <Route path="/contacts" element={<PhonebookPage />} /> */}
         </Routes>
       }
     </Suspense>
